930b1e4d37407af94b739ddbf573210416a01b98
randelramirez <randelramirez1@gmail.com>
2026-09-21 15:48:29 +0800

    Implement provider view mode selection and maximize window on activation

--- CloudExplorer/Views/ProviderPane.xaml.cs
+++ CloudExplorer/Views/ProviderPane.xaml.cs
@@ -19,6 +19,12 @@ public sealed partial class ProviderPane : UserControl
     public static readonly DependencyProperty AccentForegroundBrushProperty = DependencyProperty.Register(
         nameof(AccentForegroundBrush), typeof(Brush), typeof(ProviderPane), new PropertyMetadata(null));
 
+    public static readonly DependencyProperty IsCompactProperty = DependencyProperty.Register(
+        nameof(IsCompact),
+        typeof(bool),
+        typeof(ProviderPane),
+        new PropertyMetadata(false, OnIsCompactChanged));
+
     public ProviderPane()
     {
         InitializeComponent();
@@ -54,6 +60,12 @@ public sealed partial class ProviderPane : UserControl
         set => SetValue(AccentForegroundBrushProperty, value);
     }
 
+    public bool IsCompact
+    {
+        get => (bool)GetValue(IsCompactProperty);
+        set => SetValue(IsCompactProperty, value);
+    }
+
     private async void AccountSelectionChanged(object sender, SelectionChangedEventArgs args)
     {
         if (sender is ComboBox { SelectedItem: CloudAccountOption account } &&
@@ -61,5 +73,48 @@ public sealed partial class ProviderPane : UserControl
         {
             await viewModel.ChangeAccountAsync(account);
         }
+    }
+
+    private static void OnIsCompactChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+    {
+        if (dependencyObject is ProviderPane pane)
+        {
+            pane.UpdateResponsiveLayout((bool)args.NewValue);
+        }
+    }
+
+    private void UpdateResponsiveLayout(bool isCompact)
+    {
+        if (isCompact)
+        {
+            ControlsColumnOne.Width = new GridLength(1, GridUnitType.Star);
+            ControlsColumnTwo.Width = new GridLength(1, GridUnitType.Star);
+            ControlsColumnThree.Width = new GridLength(0);
+            ControlsColumnFour.Width = new GridLength(0);
+
+            Grid.SetRow(AccountSelector, 0);
+            Grid.SetColumn(AccountSelector, 0);
+            Grid.SetRow(GroupingSelector, 0);
+            Grid.SetColumn(GroupingSelector, 1);
+            Grid.SetRow(FilterBox, 1);
+            Grid.SetColumn(FilterBox, 0);
+            Grid.SetRow(RefreshButton, 1);
+            Grid.SetColumn(RefreshButton, 1);
+            return;
+        }
+
+        ControlsColumnOne.Width = new GridLength(1.4, GridUnitType.Star);
+        ControlsColumnTwo.Width = new GridLength(1.2, GridUnitType.Star);
+        ControlsColumnThree.Width = new GridLength(1.4, GridUnitType.Star);
+        ControlsColumnFour.Width = GridLength.Auto;
+
+        Grid.SetRow(AccountSelector, 0);
+        Grid.SetColumn(AccountSelector, 0);
+        Grid.SetRow(GroupingSelector, 0);
+        Grid.SetColumn(GroupingSelector, 1);
+        Grid.SetRow(FilterBox, 0);
+        Grid.SetColumn(FilterBox, 2);
+        Grid.SetRow(RefreshButton, 0);
+        Grid.SetColumn(RefreshButton, 3);
     }
 }
