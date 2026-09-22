76cb1759d2579b1f2d0fadf13b114d0799e61681
randelramirez <randelramirez1@gmail.com>
2026-09-22 21:46:47 +0800

    refactor: reorganize test files and namespaces for improved structure

---
+++ CloudExplorer.Tests/Infrastructure/CommandLine/ProcessCommandRunnerTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using CloudExplorer.Infrastructure.CommandLine;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+
+namespace CloudExplorer.Tests.Infrastructure.CommandLine;
+
+public class ProcessCommandRunnerTests
+{
+    [Test]
+    public async Task MissingExecutable_ReturnsActionableFailure()
+    {
+        var runner = new ProcessCommandRunner(NullLogger<ProcessCommandRunner>.Instance);
+
+        var result = await runner.RunAsync(
+            "cloud-explorer-command-that-does-not-exist",
+            [],
+            TimeSpan.FromSeconds(1));
+
+        result.IsSuccess.Should().BeFalse();
+        result.BestError.Should().Contain("was not found");
+    }
+
+    [Test]
+    public async Task UnlaunchableExecutable_DoesNotReportItAsMissing()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            Assert.Ignore("The failure mode is specific to exec on Unix.");
+            return;
+        }
+
+        var executable = Path.Combine(TestContext.CurrentContext.WorkDirectory, $"cloud-explorer-not-an-executable-{Guid.NewGuid():N}");
+        await File.WriteAllTextAsync(executable, "\u0000not a real binary");
+        File.SetUnixFileMode(executable, UnixFileMode.UserRead | UnixFileMode.UserWrite | UnixFileMode.UserExecute);
+
+        try
+        {
+            var runner = new ProcessCommandRunner(NullLogger<ProcessCommandRunner>.Instance);
+
+            var result = await runner.RunAsync(executable, [], TimeSpan.FromSeconds(5));
+
+            result.IsSuccess.Should().BeFalse();
+            result.BestError.Should().NotContain("was not found");
+            result.BestError.Should().Contain(executable);
+        }
+        finally
+        {
+            File.Delete(executable);
+        }
+    }
+}
