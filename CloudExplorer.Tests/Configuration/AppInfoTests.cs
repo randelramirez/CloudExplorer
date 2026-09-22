5c0f09dab94bf0a3a0dd00a578935be1015b8fb4
randelramirez <randelramirez1@gmail.com>
2026-09-22 21:46:47 +0800

    refactor: reorganize test files and namespaces for improved structure

---
+++ CloudExplorer.Tests/Configuration/AppInfoTests.cs
@@ -0,0 +1,22 @@
+using CloudExplorer.Configuration;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CloudExplorer.Tests.Configuration;
+
+public class AppInfoTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void AppInfoCreation()
+    {
+        var appInfo = new AppConfig { Environment = "Test" };
+
+        appInfo.Should().NotBeNull();
+        appInfo.Environment.Should().Be("Test");
+    }
+}
